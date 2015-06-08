a3f1b4af2e3762b31fb0d9c5e77b5d7c7241b900
Sam Nissen <scnissen@gmail.com>
2015-06-08 13:37:06 +0100

    Correct handling of link and image storage. Add function to get the links matching a domain.

--- lib/web_inspector/inspector.rb
+++ lib/web_inspector/inspector.rb
@@ -25,25 +25,58 @@ module WebInspector
     end
 
     def links
-      return @links if @links
+      get_new_links unless @links
+      return @links
+    end
+    
+    # View only the links to a given domain
+    # Use the page's domain as the default
+    def domain_links(user_domain = @page.host)
+      validated_domain_uri = validate_url("http://#{user_domain.downcase.gsub(/\s+/, '')}")
+      raise "Invalid domain provided" unless validated_domain_uri
       
+      domain = validated_domain_uri.host
+      
+      domain_links = []
+      
+      links.map do |l|
+        u = validate_url(l)
+        next unless u
+        
+        domain_links.push(l) if domain == u.host.downcase
+      end 
+      
+      return domain_links.compact!
+    end
+    
+    def validate_url(u)
+      begin
+        return URI.parse(u)
+      rescue URI::InvalidURIError => e
+        return false
+      end
+    end
+
+    def images
+      get_new_images unless @images
+      return @images
+    end
+
+    private
+    
+    def get_new_images
+      @images = []
+      @page.css("img").each do |img|
+        @images.push((img[:src].to_s.start_with? @url.to_s) ? img[:src] : URI.join(url, img[:src]).to_s) if (img and img[:src])
+      end
+    end
+    
+    def get_new_links
       @links = []
       @page.css("a").each do |a|
         @links.push((a[:href].to_s.start_with? @url.to_s) ? a[:href] : URI.join(@url, a[:href]).to_s) if (a and a[:href])
       end
     end
-
-    def images
-      return @images if @images
-      
-      @images = []
-      @page.css("img").each do |img|
-        @images.push((img[:src].to_s.start_with? @url.to_s) ? img[:src] : URI.join(url, img[:src]).to_s) if (img and img[:src])
-      end
-      return @images
-    end
-
-    private
 
     def snippet
       first_long_paragraph = @page.search('//p[string-length() >= 120]').first
